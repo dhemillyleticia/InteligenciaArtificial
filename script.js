6aea025300e074c65ab1a026b5ccc1faf0f30bd1
dhemillyleticia <163935697+dhemillyleticia@users.noreply.github.com>
2024-08-07 00:50:46 +0000

    lelele

---
+++ script.js
@@ -0,0 +1,7 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-pergunta");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas =
